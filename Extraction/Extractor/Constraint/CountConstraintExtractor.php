a269c411edc7c274aef12b42cbabd255c51255de
Martin Poirier Theoret <mpoiriert@gmail.com>
2015-06-25 19:02:15 -0400

    count constraint extractor

---
+++ Extraction/Extractor/Constraint/CountConstraintExtractor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Draw\Swagger\Extraction\Extractor\Constraint;
+
+use Draw\Swagger\Extraction\Extractor\ConstraintExtractor;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Count as SupportedConstraint;
+
+class CountConstraintExtractor extends ConstraintExtractor
+{
+    /**
+     * @param Constraint $constraint
+     * @return bool
+     */
+    public function supportConstraint(Constraint $constraint)
+    {
+        return $constraint instanceof SupportedConstraint;
+    }
+
+    /**
+     * @param SupportedConstraint $constraint
+     * @param ConstraintExtractionContext $context
+     */
+    public function extractConstraint(Constraint $constraint, ConstraintExtractionContext $context)
+    {
+        $this->assertSupportConstraint($constraint);
+
+        if($constraint->min) {
+            $context->propertySchema->minItems = $constraint->min;
+        }
+
+        if($constraint->max) {
+            $context->propertySchema->maxItems = $constraint->max;
+        }
+    }
+}
